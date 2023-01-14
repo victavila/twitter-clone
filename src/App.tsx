4ca414cdcc532d387ef280b2a8b1441bd34d81f7
Victor Avila <victoravila1997@gmail.com>
2023-01-14 02:49:32 -0600

    Created log in modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import LogInModal from './components/LogInModal';
 import Explore from './pages/Explore';
 
 function App() {
   return (
     <div className="App">
       <Explore />
+      <LogInModal />
     </div>
   );
 }
---
+++ src/components/LogInModal.tsx
@@ -0,0 +1,30 @@
+import { FaTwitter } from "react-icons/fa";
+import "../styles/LogInModal.css";
+
+const LogInModal = () => {
+  return (
+    <div className="modal">
+      <div className="login">
+        <div className="login__icon">
+          <FaTwitter className="icon--blue"></FaTwitter>
+        </div>
+        <h2 className="login__heading">Log in to Twitter</h2>
+        <form className="login__form">
+          <label>
+            <input type="email" placeholder="Email"></input>
+          </label>
+          <label>
+            <input type="password" placeholder="Password"></input>
+          </label>
+          <button>Log in</button>
+        </form>
+        <div className="login__footer">
+          <p>Don't have an account?</p>
+          <p>Sign up</p>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default LogInModal;
